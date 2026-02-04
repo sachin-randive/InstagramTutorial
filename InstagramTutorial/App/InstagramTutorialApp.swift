2d26b592b3e085c000c644d83636bfbf62dfda6e
Sachin Randive <r.sachin81@gmail.com>
2026-02-04 19:56:05 +0530

    User Interface - MainTabView Setup.

---
+++ InstagramTutorial/App/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by Sachin Randive on 04/02/26.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstagramTutorial/Core/TabBar/MainTabView.swift
@@ -0,0 +1,45 @@
+//
+//  MainTabView.swift
+//  InstagramTutorial
+//
+//  Created by Sachin Randive on 04/02/26.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+            
+        }
+        .accentColor(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
