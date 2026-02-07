1914c4389e12520f405bf2af3db01a7123c1c524
Sachin Randive <r.sachin81@gmail.com>
2026-02-07 08:51:57 +0530

    Profile View Minor Changes and set up.

--- InstagramTutorial/Core/Profile/ProfileView.swift
+++ InstagramTutorial/Core/Profile/ProfileView.swift
@@ -8,63 +8,90 @@
 import SwiftUI
 
 struct ProfileView: View {
+    private var gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
     var body: some View {
-        
-        VStack {
-            // header
-            VStack(spacing: 12) {
-                //pic and stats
-                HStack {
-                    Image("black_panther01")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 80, height: 80)
-                        .clipShape(Circle())
+        NavigationStack {
+            ScrollView {
+                // header
+                VStack(spacing: 12) {
+                    //pic and stats
+                    HStack {
+                        Image("black_panther01")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 8) {
+                            
+                            UserStatView(value: 3, title: "Posts")
+                            
+                            UserStatView(value: 1, title: "Followers")
+                            
+                            UserStatView(value: 2, title: "Following")
+                        }
+                    }
+                    .padding(.horizontal)
                     
-                    Spacer()
+                    // name and bio
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Sachin Randive")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Text("sachin_randive01 add your bio")
+                            .font(.footnote)
+                        
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
                     
-                    HStack(spacing: 8) {
+                    
+                    // Button Action
+                    Button {
                         
-                        UserStatView(value: 3, title: "Posts")
-                        
-                        UserStatView(value: 1, title: "Followers")
-                        
-                        UserStatView(value: 2, title: "Following")
+                    } label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 32)
+                            .foregroundColor(.black)
+                            .overlay(content: {
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(Color.black, lineWidth: 1)
+                            })
+                    }
+                    
+                    // divider
+                    Divider()
+                }
+                ///post grid view
+                
+                LazyVGrid(columns: gridItems, spacing: 1) {
+                    ForEach(0...12, id: \.self) {index in
+                        Image("black_panther01")
+                            .resizable()
+                            .scaledToFill()
                     }
                 }
-                .padding(.horizontal)
-                
-                // name and bio
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Sachin Randive")
-                        .font(.footnote)
-                        .fontWeight(.semibold)
-                    
-                    Text("sachin_randive01 add your bio")
-                        .font(.footnote)
-                    
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar  {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundStyle(.black)
+                    }
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-                
-                
-                // Button Action
-                Button {
-                    
-                } label: {
-                    Text("Edit Profile")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .frame(width: 360, height: 32)
-                        .foregroundColor(.black)
-                        .overlay(content: {
-                            RoundedRectangle(cornerRadius: 6)
-                                .stroke(Color.black, lineWidth: 1)
-                        })
-                }
-                
-                // divider
-                Divider()
             }
         }
     }
