8aa9761c48793c8e05b352144b9b1d4d1b72ebc5
Sachin Randive <r.sachin81@gmail.com>
2026-02-08 14:20:38 +0530

    User Interface - Search View Set up.

---
+++ InstagramTutorial/Core/Search/SearchView.swift
@@ -0,0 +1,51 @@
+//
+//  SearchView.swift
+//  InstagramTutorial
+//
+//  Created by Sachin Randive on 07/02/26.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    @State private var searchText: String = ""
+    var body: some View {
+        
+        NavigationStack {
+            
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(0...15, id: \.self) { user in
+                        HStack {
+                            Image("batman-2")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                            
+                            VStack(alignment: .leading) {
+                                
+                                Text("Batman Crewnel")
+                                    .fontWeight(.semibold)
+                                
+                                Text("BatmanCrewnel017")
+                            }
+                            .font(.footnote)
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search...")
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    SearchView()
+}
